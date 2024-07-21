e328c76b79f797b1d83bf86575c2a7a84a0cc8d5
MKhadijatO <muhammadkhadijato@gmail.com>
2024-07-21 17:51:44 +0100

    update

--- cypress/integration/BDD/dropdown/dropdown.js
+++ cypress/integration/BDD/dropdown/dropdown.js
@@ -3,6 +3,7 @@
 import 'cypress-iframe';
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
+
 Cypress.on('uncaught:exception', (err, runnable) => {
     // Check if the error is the one you want to ignore
     if (err.message.includes('Sys is not defined')) {
@@ -25,25 +26,16 @@ When ('I fill all the necessary fields', function(){
     cy.get("#ctl00_mainContent_ddl_originStation1_CTXT").click();
     cy.get(":nth-child(1) > :nth-child(11) > a").click(0, 0)
 
-    // //  Method 1: Using force: true with select
-    //  cy.get('#ctl00_mainContent_ddl_destinationStation1').select('DEL', { force: true });
-
-    //  // Verify that the correct value is selected
-    //  cy.get('#ctl00_mainContent_ddl_destinationStation1').should('have.value', 'DEL');
- 
-    // cy.get('#ctl00_mainContent_ddl_destinationStation1')
-    // .invoke('val', 'DEL')
-    // .trigger('change', { force: true });
-
-    // cy.get('#ctl00_mainContent_ddl_destinationStation1').should('have.value', 'DEL');
 
     cy.get("#ctl00_mainContent_ddl_destinationStation1_CTXT").click();
     cy.get("#glsctl00_mainContent_ddl_destinationStation1_CTNR > #citydropdown > tbody > :nth-child(2) > .mapbg > .search_options_menucontent > #dropdownGroup1 > .dropdownDiv > :nth-child(4) > :nth-child(4) > a").click(0, 0)
     
+    cy.wait(3000)
     cy.get("#ctl00_mainContent_view_date1").click();
-    cy.get(":nth-child(3) > :nth-child(6) > .ui-state-default").click(0, 0)
-    cy.get("#view_fulldate_id_1").should("contains.text", "Fri, Jul 19 2024")
+    cy.get(":nth-child(4) > :nth-child(1) > .ui-state-default").click(0, 0)
+    cy.get("#view_fulldate_id_1").should("contains.text", "Sun, Jul 21 2024")
    
+    cy.wait(3000)
     cy.get("#ctl00_mainContent_view_date2").click();
     cy.get(":nth-child(5) > :nth-child(4) > .ui-state-default").click();
         
@@ -59,5 +51,7 @@ When ('I fill all the necessary fields', function(){
     
 Then ('I click on Submit', function(){
     cy.get("#ctl00_mainContent_btn_FindFlights").click();
-    cy.get("#aspnetForm").submit();      
+
+    //submitting the form
+    // cy.get("#aspnetForm").submit();      
 })
